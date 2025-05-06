24cda2b7471b16df8ea2300805423c8f9fb56663
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2025-05-05 22:27:41 -0300

    resolve saudacao, somar e verificar palavra

--- src/somar.js
+++ src/somar.js
@@ -1,11 +1,20 @@
 // ## 8. Função de Soma com Erros **(1 ponto)**
 
 // **Enunciado:**
-// A função abaixo tenta somar dois números, mas está sem nenhuma validação. Encontre e corrija os problemas para que a função funcione corretamente. Considere que o **parâmetro b pode ser opcional** e que, se não for fornecido, **deve ser considerado como 0**. Além disso, **a função não deve retornar valores de tipos inesperados (como NaN ou undefined)**, e deve tratar casos em que os **parâmetros não são números**, retornando a mensagem "Parâmetro inválido".
+// A função abaixo tenta somar dois números, mas está sem nenhuma validação. 
+// Encontre e corrija os problemas para que a função funcione corretamente. 
+// Considere que o **parâmetro b pode ser opcional** e que, se não for fornecido, 
+// **deve ser considerado como 0**. Além disso, **a função não deve retornar valores de
+// tipos inesperados (como NaN ou undefined)**, e deve tratar casos em que os **parâmetros 
+// não são números**, retornando a mensagem "Parâmetro inválido".
 
-function somar(a, b) {
-  let resultado = a + b;
-  return resultado;
+function somar(a, b = 0) {
+  const tryNumberA = Number(a);
+  const tryNumberB = Number(b);
+  if (isNaN(tryNumberA) || isNaN(tryNumberB)) {
+    return "Parâmetro inválido";
+  }
+  return tryNumberA + tryNumberB;
 }
 
   
